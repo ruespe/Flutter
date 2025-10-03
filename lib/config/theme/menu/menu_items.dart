80fce77945976fad577781e18584fd35ead115b0
ruben <rubenespin84@gmail.com>
2025-10-03 12:29:47 +0200

    Menu Items

---
+++ lib/config/theme/menu/menu_items.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class MenuItem {
+  final String title;
+  final String subTitle;
+  final String link;
+  final IconData icon;
+
+  const MenuItem({
+    required this.title,
+    required this.subTitle,
+    required this.link,
+    required this.icon,
+  });
+}
+
+const appMenuItems = <MenuItem>[
+  MenuItem(
+    title: 'Botones',
+    subTitle: 'Varios botones en Flutter',
+    link: '/butones',
+    icon: Icons.smart_button_outlined,
+  ),
+  MenuItem(
+    title: 'Botones',
+    subTitle: 'Un contenedor estilizado',
+    link: '/butones',
+    icon: Icons.credit_card,
+  ),
+];
