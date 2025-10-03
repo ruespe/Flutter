b176ce353bb0e09f34abc1a79952a722a6251214
ruben <rubenespin84@gmail.com>
2025-10-03 12:30:05 +0200

    Home Screen creado

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/presentation/screens/home/home_screen.dart';
+import '/config/theme/app_theme.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +11,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('VIVA EL BETIS!'),
-        ),
-      ),
-    );
-  }
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme(selectedColor: 2).getTheme(),
+      home: const HomeScreen()
+      );
+    }
 }
