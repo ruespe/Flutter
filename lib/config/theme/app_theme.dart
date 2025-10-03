24ed252ba05440c475b17c98f60cfed69dd28ed0
ruben <rubenespin84@gmail.com>
2025-10-03 12:29:19 +0200

    Menu Items

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.red,
+  Colors.purple,
+  Colors.deepPurple,
+  Colors.orange,
+  Colors.pink,
+  Colors.pinkAccent,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0}):assert(selectedColor >= 0 && selectedColor < colorList.length, 'Selected color must be greater than 0 and dont surpass colorList length'  );
+
+  ThemeData getTheme() => ThemeData(useMaterial3: true, 
+  colorSchemeSeed: colorList[selectedColor],
+  appBarTheme: const AppBarTheme(centerTitle: true)
+  );
+}
