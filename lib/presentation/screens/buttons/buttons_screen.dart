fd4db006975d93b47d98979a2b171986f9da43a6
ruben <rubenespin84@gmail.com>
2025-10-07 09:51:50 +0200

    Botones PreConfigurados

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class ButtonsScreen extends StatelessWidget {
-
   static const name = 'buttons_screen';
 
   const ButtonsScreen({super.key});
@@ -10,12 +9,10 @@ class ButtonsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Buttons Screen'),
-      ),
+      appBar: AppBar(title: const Text('Buttons Screen')),
       body: _ButtonsView(),
       floatingActionButton: FloatingActionButton(
-        child:  Icon(Icons.arrow_back_ios_new_rounded),
+        child: Icon(Icons.arrow_back_ios_new_rounded),
         onPressed: () {
           context.pop();
         },
@@ -25,19 +22,55 @@ class ButtonsScreen extends StatelessWidget {
 }
 
 class _ButtonsView extends StatelessWidget {
-  const _ButtonsView( );
+  const _ButtonsView();
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-      child: Wrap(
-        spacing: 10,
-        children: [
-          ElevatedButton(onPressed: () {}, child: const Text('Elevated')),
-          const ElevatedButton(onPressed: null, child: Text('Elevated Disabled')),
-          ElevatedButton.icon(onPressed: () {}, label: const Text('Elevated Icon'))
-        ],
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+        child: Wrap(
+          spacing: 10,
+          children: [
+            ElevatedButton(onPressed: () {}, child: const Text('Elevated')),
+            const ElevatedButton(
+              onPressed: null,
+              child: Text('Elevated Disabled'),
+            ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.access_alarm_rounded),
+              label: const Text('Elevated Icon'),
+            ),
+            FilledButton(onPressed: () {}, child: const Text('Filled')),
+            FilledButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.account_circle_outlined),
+              label: const Text('Filled Icon'),
+            ),
+            OutlinedButton(onPressed: () {}, child: const Text('Outlined')),
+            OutlinedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.adb_rounded),
+              label: const Text('Usuarios'),
+            ),
+            TextButton(onPressed: () {}, child: const Text('Text Button')),
+            TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.accessibility_new),
+              label: const Text('Nuevos'),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.volume_up_rounded),
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(Colors.blue.shade100), // MaterialStateProperty.all is deprecated usar nueva versión
+                iconColor: const WidgetStatePropertyAll(Colors.blue),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
